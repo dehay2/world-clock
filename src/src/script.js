858a3673b0718d0125250f681597db0f277692f5
dehay2 <dejapatterson2698@gmail.com>
2026-01-04 23:30:47 -0500

    Added times and updated html/js

--- src/src/script.js
+++ src/src/script.js
@@ -1,1 +1,24 @@
+function updateTime() {
+  let losAngelesElement = document.querySelector("#losAngeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTimeZone = moment().tz("America/Los_Angeles");
 
+  losAngelesDateElement.innerHTML = losAngelesTimeZone.format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTimeZone.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let sydneyElement = document.querySelector("#sydney");
+  let sydneyDateElement = sydneyElement.querySelector(".date");
+  let sydneyTimeElement = sydneyElement.querySelector(".time");
+  let sydneyTimeZone = moment().tz("Australia/Sydney");
+
+  sydneyDateElement.innerHTML = sydneyTimeZone.format("MMMM Do YYYY");
+  sydneyTimeElement.innerHTML = sydneyTimeZone.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1);
